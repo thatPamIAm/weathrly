32c96866b1a9b7b281de5561c73a67d523b71e56
Michael Fenwick <mickyfen17@aol.com>
2017-02-25 16:31:34 -0700

    add further tests to NavBar

--- test/test.js
+++ test/test.js
@@ -1,29 +1,76 @@
 import { expect } from 'chai';
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../lib/components/App'
+import NavBar from '../lib/components/NavBar'
 
-describe('testing weathrly', () => {
-  it('should have a component named NavBar', () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.find('NavBar')).to.have.length(1);
+describe('testing Weathrly App', () => {
+
+  describe('testing NavBar component', () => {
+    it('should have a component named NavBar', () => {
+      const wrapper = shallow(<App/>);
+      expect(wrapper.find('NavBar')).to.have.length(1);
+    });
+
+    it('NavBar should contain an input feild and a submit button', () => {
+      const wrapper = shallow(<NavBar/>);
+      expect(wrapper.find('.textInput')).to.have.length(1);
+      expect(wrapper.find('.submitBtn')).to.have.length(1);
+    });
+
+    it('NavBar should contain a state of location', () => {
+      const wrapper = shallow(<NavBar/>);
+      const navBarStates= wrapper.state();
+
+      expect(navBarStates.location).to.equal('');
+    });
+
+    it.skip('should be able to change the location state of NavBar', () => {
+      const wrapper = shallow(<NavBar/>);
+      const navBarStates= wrapper.state();
+      const inputField = wrapper.find('.textInput');
+
+      expect(navBarStates.location).to.equal('');
+      inputField.simulate('change', {target: {value: 'Denver CO'}});
+      expect(navBarStates.location).to.equal('Denver, CO');
+    });
+
+    it.skip('should be able to change the location state of NavBar and on submit clear the location state', () => {
+      const wrapper = shallow(<NavBar/>);
+      const navBarStates= wrapper.state();
+      const inputField = wrapper.find('.textInput');
+      const submitBtn = wrapper.find('.submitBtn');
+
+      expect(navBarStates.location).to.equal('');
+      inputField.simulate('change', {target: {value: 'Denver, CO'}});
+      expect(navBarStates.location).to.equal('Denver, CO');
+      submitBtn.simulate('click');
+      expect(navBarStates.location).to.equal('');
+    });
+
   });
 
-  it('should have a component named CurrentWeatherCard', () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.find('CurrentWeatherCard')).to.have.length(1);
+  describe('testing CurrentWeatherCard component', () => {
+    it('should have a component named CurrentWeatherCard', () => {
+      const wrapper = shallow(<App/>);
+      expect(wrapper.find('CurrentWeatherCard')).to.have.length(1);
+    });
+
   });
 
-  it('should have a component named TenDay', () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.find('TenDay')).to.have.length(1);
+  describe('testing TenDay component', () => {
+    it('should have a component named TenDay', () => {
+      const wrapper = shallow(<App/>);
+      expect(wrapper.find('TenDay')).to.have.length(1);
+    });
+
   });
 
-  it.skip('should have a component named Hourly', () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.find('Hourly')).to.have.length(1);
+  describe('testing Hourly component', () => {
+    it.skip('should have a component named Hourly', () => {
+      const wrapper = shallow(<App/>);
+      expect(wrapper.find('Hourly')).to.have.length(1);
+    });
+
   });
-
-
-
 });
