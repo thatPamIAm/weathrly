f72e835d6e8a50a2fd1281c4b1af4f549bb6778c
Michael Fenwick <mickyfen17@aol.com>
2017-02-25 14:11:13 -0700

    add basic component check within App

--- test/test.js
+++ test/test.js
@@ -1,12 +1,27 @@
 import { expect } from 'chai';
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import App from '../lib/components/App'
 
-describe('testing with enzyme', () => {
-  it('testing test file', () => {
-    // const wrapper = shallow(<Test/>);
-    // console.log(wrapper.debug());
-    // expect(wrapper.find('Text')).to.have.length(1);
+describe('testing weathrly', () => {
+  it('should have a component named NavBar', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('NavBar')).to.have.length(1);
+  });
+
+  it('should have a component named CurrentWeatherCard', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('CurrentWeatherCard')).to.have.length(1);
+  });
+
+  it('should have a component named TenDay', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('TenDay')).to.have.length(1);
+  });
+
+  it.skip('should have a component named Hourly', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('Hourly')).to.have.length(1);
   });
 
 
