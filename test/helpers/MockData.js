17c4c6548a1de43b8cff387c8ce34ed4654cceaf
Michael Fenwick <mickyfen17@aol.com>
2017-02-25 21:08:05 -0700

    add mock data for TenDay testing

--- test/helpers/MockData.js
+++ test/helpers/MockData.js
@@ -25,5 +25,32 @@ const ForecastData = {
   }
 };
 
-export default { CurrentData, ForecastData };
+const TenDay = {
+  simpleforecast: {
+    forecastday: [{
+      date: {
+        weekday_short: 'Tue'
+      },
+      high: {
+        fahrenheit: 65
+      },
+      low: {
+        fahrenheit: 28
+      }
+    },
+    {
+      date: {
+        weekday_short: 'Wed'
+      },
+      high: {
+        fahrenheit: 58
+      },
+      low: {
+        fahrenheit: 20
+      }
+    }]
+  }
+};
+
+export default { CurrentData, ForecastData, TenDay };
 
